f8771dab49e729a4e0f70fcf9a8ef751e0d50de4
Avanish-Yadav <avanishyadav1906@gmail.com>
2024-06-02 23:57:56 +0530

    new code

--- server.js
+++ server.js
@@ -9,24 +9,22 @@ const person=require('./models/person');
 app.get('/', function (req, res) {
   res.send('Hello Avanish');
 })
-app.post('/person',(req,res)=>{
-
+app.post('/person',async(req,res)=>{
+try{
   const data=req.body// Assuming the request body contains the person data
   //Create a new Person document using the Mongoose model
   const newPerson=new person(data);
 
   //save the new person to the database
-  newPerson.save((error,savedPerson)=>{ //but now a time callback is not used but async await is used
-      if(error){
-        console.log('Error saving person',error);
-        res.status(500).json({error:'Internal server error'});
-      }else{
-        console.log('data saved successfully');
-        res.status(200).json(savedPerson);
-      }
-  })
-  
+    const response=await newPerson.save()
+    console.log('data saved');
+      res.status(200).json(response);
 
+}catch(err){
+    console.log(err);
+    res.status(500).json({error:'Internal server error'});
+  }
+  
 })
 app.listen(5000,()=>{
   console.log("server is listening at port 5000");
